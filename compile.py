9022dd45787e26d399793c3d4cbe63adf4a79435
ttzytt <Zyt2006613@outlook.com>
2025-02-06 16:01:27 -0500

    added compilation into standalone file for lime light

---
+++ compile.py
@@ -0,0 +1,16 @@
+import stickytape
+
+def compile(path : str):
+    output = stickytape.script(
+        path = path, 
+        add_python_paths=["./src"], 
+    )
+    # make the output to be /build/[path]
+    # remove .py but not the file name of the path
+    path = path.split("/")[-1].replace(".py", "")
+    output_path = f"./build/{path}_standalone.py"
+    with open(output_path, "w") as f:
+        f.write(output)
+
+if __name__ == "__main__":
+    compile("./main.py")
